4503def31bbdf67b96b9fbf6d317a1871e576cff
xtt <xiongtaotao@jiuhuar.com>
2018-08-09 18:29:49 +0800

    [xttblog-cloud]新增xttblog-ribbon-hystrix

--- xttblog-cloud-producer2/src/main/java/com/xttblog/controller/CalController.java
+++ xttblog-cloud-producer2/src/main/java/com/xttblog/controller/CalController.java
@@ -1,4 +1,4 @@
-package xttblog.controller;
+package com.xttblog.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ xttblog-ribbon-hystrix/src/main/java/com/xttblog/service/CalService.java
@@ -0,0 +1,11 @@
+package com.xttblog.service;
+
+/**
+ * CalService
+ *
+ * @author xtt
+ * @date 2018/8/9 上午10:10
+ */
+public interface CalService {
+    Integer add(Integer a, Integer b);
+}
